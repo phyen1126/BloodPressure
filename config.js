0ad9aac557d1403ec1b3a55471b1648b4ae362bd
phyen1126 <phyen1126@gmail.com>
2026-07-22 18:23:11 +0800

    V10

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ CLIENT_ID:"482226624163-gdvcduad8avodq5p6hb6e9m2gcrpn5h5.apps.googleusercontent.
 API_KEY:"AIzaSyCgF6AGv8uyuBkipFdUS3bfre_nV-6Ck5A",
 APP_ID:"482226624163",
 SCOPE:"https://www.googleapis.com/auth/drive.file",
-FILE_NAME:"BloodPressure.csv"
+FILE_NAME:"BloodPressure.csv",META_FILE_NAME:"BloodPressure.sync.json"
 });
